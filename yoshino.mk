033ab493ca2b055da41d75cd089fc8f74189f6e8
Tuan Anh <tuan73176@gmail.com>
2026-04-21 16:11:53 +0000

    yoshino-common: Build AIDL LiveDisplay HAL
    Change-Id: I5f599dde09c04ce92e7e8f9e009a1c38675968d7

--- yoshino.mk
+++ yoshino.mk
@@ -80,6 +80,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.lineage
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay-service.sdm
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
