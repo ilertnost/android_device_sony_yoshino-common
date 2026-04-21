f59621e1cb012c922fee0aa9cc163697cd3ab09f
LuK1337 <priv.luk@gmail.com>
2026-04-21 16:24:58 +0000

    yoshino-common: Build QTI telephony
    Change-Id: Ie97aac4aa910d3b00f13aec6764e8be3e277e71c

--- yoshino.mk
+++ yoshino.mk
@@ -189,6 +189,19 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client
 
+# Telephony
+PRODUCT_PACKAGES += \
+    ims-ext-common \
+    ims_ext_common.xml \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb
