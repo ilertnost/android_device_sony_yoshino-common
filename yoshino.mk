63ed576d1abe091db279b709225eecd9f0e35a30
LuK1337 <priv.luk@gmail.com>
2026-04-24 16:35:43 +0000

    yoshino-common: Sync and build SimDetect
    Change-Id: Ia9a47be3627a7ebd63f241feee0ed3479241967d

--- yoshino.mk
+++ yoshino.mk
@@ -211,6 +211,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl:64 \
     android.hardware.sensors@1.0-service
 
+# SimDetect
+PRODUCT_PACKAGES += \
+    SimDetect
+
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
