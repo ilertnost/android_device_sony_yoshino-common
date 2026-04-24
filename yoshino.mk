d7630551b0e6313fd228e123ea93dd0450027ece
Tuan Anh <tuan73176@gmail.com>
2026-04-24 15:58:53 +0000

    yoshino-common: Import overlays from Nile
    Snapshot of:
    - overlay/: LineageOS/android_device_sony_nile-common@1cb2a1578f62b7616a3f9ec750fb9231cc3836be
    - overlay-lineage/: LineageOS/android_device_sony_nile-common@bb6bc9d658659e2a34b365df03eacc18cf2c58a1
      with 2b42103 and bb6bc9d being reverted.
    - rro_overlays/: LineageOS/android_device_sony_nile-common@3f7e4a2fbbb3deab49c4bce54a570ad4120301f4
    
    Change-Id: If2b0beec4dc0aa232d0fdc0e687d6ac281973a82

--- yoshino.mk
+++ yoshino.mk
@@ -115,6 +115,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
+
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
@@ -226,6 +233,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# Tethering
+PRODUCT_PACKAGES += \
+    TetheringConfigOverlay
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb
@@ -247,7 +258,8 @@ PRODUCT_PACKAGES += \
     libwifi-hal-ctrl \
     libwpa_client \
     wpa_supplicant \
-    wpa_supplicant.conf
+    wpa_supplicant.conf \
+    WifiOverlay
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
