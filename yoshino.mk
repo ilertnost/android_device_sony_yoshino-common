0a2b7bf38225a32a17587147eab9a52919cc7762
ilert <ilamoiseev757@gmail.com>
2026-08-18 09:28:29 +0300

    yoshino-common: disable VINTF kernel requirement enforcement
    Kernel 4.4 predates VINTF kernel FCM requirements for Android 16.

--- yoshino.mk
+++ yoshino.mk
@@ -8,7 +8,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
-PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := true
+# Kernel 4.4 predates VINTF kernel FCM requirements
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
 
 # Audio
 PRODUCT_PACKAGES += \
